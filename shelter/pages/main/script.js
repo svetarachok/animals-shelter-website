97fa7fd82a28dae5b27925a2653b12c91d38298d
Sveta Rachok <sveta.r1212@gmail.com>
2022-04-21 17:56:07 +0300

    feat: add shadowed overlay to opened menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,18 +1,20 @@
 const burgerMenu = document.querySelector('.burger-icon');
 const menuLinks = document.querySelectorAll('.nav_item');
 const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay')
 
 console.log(menuLinks)
 
 let openMenu = () => {
     burgerMenu.classList.add('clicked');
     menu.classList.add('opened');
-
+    overlay.classList.add('shadowed')
 }
 
 let closeMenu = () => {
     burgerMenu.classList.remove('clicked');
     menu.classList.remove('opened');
+    overlay.classList.remove('shadowed')
 }
 
 burgerMenu.addEventListener('click', function() {
