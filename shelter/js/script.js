1373a75dce285ec575cb83e5a9a4c30aafc30287
Sveta Rachok <sveta.r1212@gmail.com>
2022-04-23 15:19:11 +0300

    feat: add slider move left and right on buttons click

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -35,12 +35,21 @@ overlay.addEventListener('click', closeMenu)
 
 const getData = async (url) => await ( await fetch (url)).json();
 const petsDataArray = await getData('../../js/pets.json')
+const BTN_SLIDER_LEFT = document.querySelector('#btn-slider-left');
+const BTN_SLIDER_RIGHT = document.querySelector('#btn-slider-right');
+const SLIDER = document.querySelector('#slider');
 
 console.log(petsDataArray)
 
-const clearSlidesContainer = () => {
-    const slidesContainer = document.querySelectorAll('.slider > .slider')
-    slidesContainer.forEach(container => container.innerHTML = '');
+// const clearSlidesContainer = () => {
+//     const slidesContainer = document.querySelectorAll('.slider > .slider')
+//     slidesContainer.forEach(container => container.innerHTML = '');
+//     return slidesContainer
+// }
+
+const clearSlidesContainer = (position) => {
+    const slidesContainer = document.querySelector(`.slider-${position}`)
+    slidesContainer.innerHTML = '';
     return slidesContainer
 }
 
@@ -52,20 +61,58 @@ const generateCards = (data) => {
     return cards;
 }
 
-let getRandom = () => Math.floor(Math.random()*8);
 
 const renderSliderCards = (dataArray) => {
     let slider = clearSlidesContainer();
     let cardsArray = generateCards(dataArray);
     console.log(cardsArray)
     for(let i = 0; i < 3; i++) {
-        slider.forEach(container => container.append(cardsArray[`${getRandom()}`].generateCard()))
+        slider.forEach(container => container.append(cardsArray[i].generateCard()))
     }
 }
 
 // renderSliderCards(petsDataArray)
 
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arrPrev = [2, 6, 8];
 
+// const generateArr = (arr) => {
+//     let arrPrev = [2, 6, 8];
+//     let arrNew = [];
+//     const rand = () => Math.floor(Math.random() * arr.length);
+    
+//     for (let i = 0; i < 3; i++) {        
+//         let randCardnumber = rand();
+//         if (!arrPrev.includes(randCardnumber) && !arrNew.includes(randCardnumber)) {
+//             arrNew.push(randCardnumber)
+//         } else {
+//            i-- 
+//         }
+//     }
+//     return arrNew
+// }
 
+// generateArr(arr)
 
+const moveLeft = () => {
+    SLIDER.classList.add(`transition-left`)
+    BTN_SLIDER_LEFT.removeEventListener('click', moveLeft)
+    BTN_SLIDER_RIGHT.removeEventListener('click', moveRight)
+}
+
+const moveRight = () => {
+    SLIDER.classList.add(`transition-right`)
+    BTN_SLIDER_LEFT.removeEventListener('click', moveLeft)
+    BTN_SLIDER_RIGHT.removeEventListener('click', moveRight)
+}
+
+BTN_SLIDER_LEFT.addEventListener('click', moveLeft)
+BTN_SLIDER_RIGHT.addEventListener('click', moveRight)
+
+SLIDER.addEventListener('animationend', () => {
+    SLIDER.classList.remove('transition-left');
+    SLIDER.classList.remove('transition-right');
+    BTN_SLIDER_LEFT.addEventListener('click', moveLeft);
+    BTN_SLIDER_RIGHT.addEventListener('click', moveRight);   
+})
 
